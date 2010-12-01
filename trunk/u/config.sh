1322aaf43d98803fc310bbe4b26911abedd14699
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2010-12-01 13:46:09 +0000

    Added repo installer

--- trunk/u/config.sh
+++ trunk/u/config.sh
@@ -16,17 +16,12 @@ then
   . "$DDIR"/../config.sh
 fi
   
-# Defaults
-USER=${USER:-hudson}
-LANG=
-
 # Software URL Bases
 GNU_URL=${GNU_URL:-http://mirrors.kernel.org/gnu}
 
 # Calculated varaibles
 SSH_ID=${SSH_ID:-$USER@$SSH_SERVER}
 SSH_PORT=${SSH_PORT:-22}
-SVN_SSH="ssh -p $SSH_PORT"
 
 ARCH=`arch`
 if test "$ARCH" = i686
@@ -36,11 +31,15 @@ elif test "$ARCH" = x86_64
 then
   ARCH_BITS=64 
 fi
-  
-PATH="$DDIR/usr/bin:$DDIR/bin:/bin:/usr/bin" # installed tools have a priority
-LDFLAGS="-L$QDIR/usr/lib"
-CPPFLAGS="-I$QDIR/usr/include"
-LD_LIBRARY_PATH="$DDIR/usr/lib"
 
-export LANG PATH LDFLAGS CPPFLAGS LD_LIBRARY_PATH SVN_SSH
+# Setting PATH so installed tools have a priority
+test "$CLEAN_ENV" = true || exec env -i CLEAN_ENV=true \
+  USER=${USER:-hudson} \
+  LANG='' \
+  PATH="$DDIR/usr/bin:$DDIR/bin:/bin:/usr/bin" \
+  LDFLAGS="-L$QDIR/usr/lib" \
+  CPPFLAGS="-I$QDIR/usr/include" \
+  LD_LIBRARY_PATH="$DDIR/usr/lib" \
+  SVN_SSH="ssh -p $SSH_PORT" \
+  sh -evx "$0" "$@"
 
