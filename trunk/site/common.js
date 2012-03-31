485e837c2b0d7ddde1085e5694cc1300192be790
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2012-03-31 20:24:10 +0000

    debug

--- trunk/site/common.js
+++ trunk/site/common.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $.getScript('//www.google-analytics.com/ga.js')
 
   // Share Buttons
-  $.getScript('//connect.facebook.net/ru_RU/all.js#xfbml=1')
+  //$.getScript('//connect.facebook.net/ru_RU/all.js#xfbml=1')
   $.getScript('https://apis.google.com/js/plusone.js')
   $.getScript('//platform.twitter.com/widgets.js')
   $.getScript('http://assets.pinterest.com/js/pinit.js')
