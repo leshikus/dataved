72288c97a80ba74a6d7408d6aa339802712f3861
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2012-03-31 18:51:20 +0000

    Added like

--- site/common.js
+++ site/common.js
@@ -1,4 +1,6 @@
 // Google Analytics
 var _gaq = [['_setAccount', 'UA-13024987-1'], ['_trackPageview']]
-$.getScript('http://www.google-analytics.com/ga.js')
+$.getScript('http://www.google-analytics.com/ga.js', function() {
+  $.getScript('http://connect.facebook.net/ru_RU/all.js#xfbml=1', function() {})
+})
 
