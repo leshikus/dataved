d513d921807f2ee18a16060bbbaa19158bc55609
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2012-03-31 20:38:57 +0000

    facebook code

--- site/common.js
+++ site/common.js
@@ -1,10 +1,18 @@
+(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) {return;}
+js = d.createElement(s); js.id = id;
+js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 $(document).ready(function() {
   // Google Analytics
   var _gaq = [['_setAccount', 'UA-13024987-1'], ['_trackPageview']]
   $.getScript('//www.google-analytics.com/ga.js')
 
   // Share Buttons
-  $.getScript('//connect.facebook.net/ru_RU/all.js#xfbml=1')
+  //$.getScript('//connect.facebook.net/ru_RU/all.js#xfbml=1')
   $.getScript('https://apis.google.com/js/plusone.js')
   $.getScript('//platform.twitter.com/widgets.js')
   $.getScript('http://assets.pinterest.com/js/pinit.js')
