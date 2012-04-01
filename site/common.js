ef7e4794a46c1ea0306e4285dd961fbbfb57bdf3
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2012-04-01 10:40:21 +0000

    debug

--- site/common.js
+++ site/common.js
@@ -6,12 +6,22 @@ js.src = "http://connect.facebook.net/en_US/all.js#xfbml=1";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-13024987-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 $(document).ready(function() {
   // Google Analytics
-  var _gaq = [['_setAccount', 'UA-13024987-1'], ['_trackPageview']]
-  $.getScript('//www.google-analytics.com/ga.js', function() {
-    console.log('loadad GA')
-  })
+  //var _gaq = [['_setAccount', 'UA-13024987-1'], ['_trackPageview']]
+  //$.getScript('//www.google-analytics.com/ga.js', function() {
+//    console.log('loadad GA')
+//  })
 
   var plusone_loaded = (typeof gapi_init == 'undefined') ? function() {} : function() {
     $.getScript("https://apis.google.com/js/client.js?onload=gapi_init")
