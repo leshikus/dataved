4868c7ec208748880d404fc958f67ea3c2494f9a
frexin@gmail.com <frexin@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2012-04-16 22:28:25 +0000

    loading video and playing queue

--- site/youtube/jquery.youtube.js
+++ site/youtube/jquery.youtube.js
@@ -3,8 +3,92 @@
     var methods = {
         init : function (options) {
 
-            return this.each(function () {
+            var defaults = {
+                'query' : 'blues',
+                'results_count' :  25
+            };
 
+            var playlist = [];
+            var cur_index = 0;
+            var player = null;
+            var player_is_loaded = false;
+
+            var opts = $.extend(options, defaults);
+            var url = 'http://gdata.youtube.com/feeds/api/videos?alt=json&q=' + defaults.query;
+
+            var container_id = $(this).attr('id');
+
+            var loadPlaylist = function(cbk) {
+                $.getJSON(url, function(json) {
+                    var entries = json.feed.entry;
+
+                    for (var i = 0; i < entries.length; i++) {
+                        var obj = entries[i];
+                        playlist.push({
+                            'id' : getVideoId(obj.id.$t),
+                            'title' : obj.title.$t
+                        });
+                    }
+
+                    cbk();
+                });
+            };
+
+            var getVideoId = function(str) {
+                var matches = /videos\/([\S]+)/.exec(str);
+                var video_id = matches[1];
+
+                return video_id;
+            };
+
+            var playerCallback = function() {
+                var state = player.getPlayerState();
+                if (0 == state) {
+                    cur_index++;
+                    goTo(cur_index);
+                }
+            };
+
+            var initPlayer = function(video_id, callback) {
+                var params = { allowScriptAccess: "always" };
+                var player_id = 'youtube-' + container_id;
+                var atts = { id: player_id };
+
+                swfobject.embedSWF("http://www.youtube.com/v/" + video_id + "?enablejsapi=1&playerapiid=ytplayer",
+                    container_id, "425", "356", "8", null, null, params, atts);
+
+                var player = document.getElementById(player_id);
+
+                var interval = window.setInterval(function () {
+
+                    try {
+                        if (player_is_loaded) {
+                            callback();
+                            player.addEventListener("onStateChange", "playerCallback");
+                            window.clearInterval(interval);
+                        }
+                        player_is_loaded = player.getVideoEmbedCode();
+
+                    } catch (e) {
+                    }
+                }, 100);
+
+                return player;
+            };
+
+            var goTo = function(index) {
+                var video_id = playlist[index].id;
+                player.loadVideoById(video_id);
+            };
+
+            return this.each(function () {
+                loadPlaylist(function() {
+                    player = initPlayer(playlist[0].id, function() {
+                        goTo(0);
+
+                        window.setInterval(playerCallback, 1000);
+                    });
+                });
             });
 
         },
