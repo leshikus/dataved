9172ff499bdb14a9d07d90f677688d9c7e791f58
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2012-12-30 18:26:12 +0000

    Added ga_loaded callback

--- site/common.js
+++ site/common.js
@@ -1,23 +1,22 @@
 (function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) {return;}
-js = d.createElement(s); js.id = id;
-js.src = "http://connect.facebook.net/ru_RU/all.js#xfbml=1";
-fjs.parentNode.insertBefore(js, fjs);
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "http://connect.facebook.net/ru_RU/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-var _gaq
+var _gaq;
 
 $(document).ready(function() {
   // Google Analytics
-  _gaq = [['_setAccount', 'UA-13024987-1'], ['_trackPageview']]
-  $.getScript('//www.google-analytics.com/ga.js')
+  _gaq = [['_setAccount', 'UA-13024987-1'], ['_trackPageview']];
+  $.getScript('//www.google-analytics.com/ga.js', ga_loaded);
 
   var plusone_loaded = (typeof gapi_init == 'undefined') ? function() {} : function() {
-    $.getScript("https://apis.google.com/js/client.js?onload=gapi_init")
+    $.getScript("https://apis.google.com/js/client.js?onload=gapi_init");
   }
 
   // Share Buttons
-  $.getScript('https://apis.google.com/js/plusone.js', plusone_loaded)
-  $.getScript('//platform.twitter.com/widgets.js')
-})
+  $.getScript('https://apis.google.com/js/plusone.js', plusone_loaded);
+});
