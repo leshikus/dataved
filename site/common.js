0f019e102d50b73889f10881ffe7d77ec531e09a
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2012-12-31 13:27:13 +0000

    Removed extra var

--- site/common.js
+++ site/common.js
@@ -10,21 +10,18 @@ var _gaq;
 
 $(document).ready(function() {
   // Google Analytics
-  console.log('_gaq = ' + _gaq);
-  var _gaq = _gaq || [];
+  _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-13024987-1']);
   _gaq.push(['_trackPageview']);
 
-  //var _gaq = [['_setAccount', 'UA-13024987-1'], ['_trackPageview']];
-
   $.getScript('//www.google-analytics.com/ga.js');
-/*
+
   window.onerror = function(msg, url, line) {
     var preventErrorAlert = true;
     _gaq.push(['_trackEvent', 'JS Error', msg, navigator.userAgent + ' -> ' + url + " : " + line, 0, true]);
     return preventErrorAlert;
   };
-*/
+
   var plusone_loaded = (typeof gapi_init == 'undefined') ? function() {} : function() {
     $.getScript("https://apis.google.com/js/client.js?onload=gapi_init");
   }
