0561546c53a7505e98e2f5c93b34ba3f81dab449
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2012-12-31 13:02:02 +0000

    v1

--- site/common.js
+++ site/common.js
@@ -12,16 +12,16 @@ $(document).ready(function() {
   // Google Analytics
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-13024987-1']);
-  _gaq.push(['_trackPageview']);  
+  _gaq.push(['_trackPageview']);
 
   $.getScript('//www.google-analytics.com/ga.js');
-
+/*
   window.onerror = function(msg, url, line) {
     var preventErrorAlert = true;
     _gaq.push(['_trackEvent', 'JS Error', msg, navigator.userAgent + ' -> ' + url + " : " + line, 0, true]);
     return preventErrorAlert;
   };
-
+*/
   var plusone_loaded = (typeof gapi_init == 'undefined') ? function() {} : function() {
     $.getScript("https://apis.google.com/js/client.js?onload=gapi_init");
   }
