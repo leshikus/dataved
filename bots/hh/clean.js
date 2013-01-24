c846ca7e37e4a16262592e0febd3d16a20826797
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2013-01-24 19:38:50 +0000

    Cleaning duplicate emails from the list

---
+++ bots/hh/clean.js
@@ -0,0 +1,23 @@
+// res = 
+
+
+function isEnglish(name) {
+  return name.replace(/[a-zA-Z\s]*/g, '') == '';
+}
+
+var e, eng = [], rus = [], out = [], r = [];
+
+while (e = res.shift()) (isEnglish(e[1]) ? eng : rus).push(e);
+
+while (e = rus.shift()) {
+  if (!out[e[2]]) r.push(e);
+  out[e[2]] = 1;
+}
+
+while (e = eng.shift()) {
+  if (!out[e[2]]) r.push(e);
+  out[e[2]] = 1;
+}
+
+console.log(r.length);
+JSON.stringify(r);
