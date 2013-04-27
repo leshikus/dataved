6cf5eb42d988993674843f528bf2b871e46cd142
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2013-04-27 13:20:49 +0000

    v1

--- bots/hh/collects.js
+++ bots/hh/collects.js
@@ -1,6 +1,3 @@
-var skipNames = [];
-skipNames = arrToHash(skipNames);
-
 var TIMEOUT = 500;
 var ERR_RELOAD = 'Произошла ошибка. Мы скоро это починим.';
 var RESUME_RENEWED = /(Резюме обновлено)\s.*\s(\d+\.\d+\.\d+)/;
@@ -15,6 +12,19 @@ function waitFor(f) {
       waitFor(f);
     }
   }, TIMEOUT);
+}
+
+function pushRes(res, type, name, email, href, text) {
+  res.push([type, name, email, href.replace(/\?.*/, ''), text]);
+  // nextW(i - 1);
+  www = window.open('https://script.google.com/macros/s/AKfycbwQ0KbAXlSCinvUjFr__kAv7cEwmkuix1wjEjsByq1yoNpsYEY/exec?name=' +
+                    encodeURIComponent(type + ':' + name + ':' + email) +
+                    '&content=' + encodeURIComponent(text));
+  waitFor(function() {
+    console.log(www.document);
+    www.close();
+    // nextW(i - 1);
+  });
 }
 
 function arrToHash(arr) {
@@ -54,10 +64,6 @@ function nextW(i) {
   var typeElem = elem.children[1].children[1].children[3];
   var type = typeElem.innerText;
   var name = elem.children[1].children[1].children[4].innerText;
-  if (skipNames[type + '::' + name]) {
-    nextW(i - 1);
-    return;
-  }
   
   if (d[i].className.indexOf('output__item_visited') >= 0) {
     nextW(i - 1);
@@ -81,36 +87,35 @@ function nextW(i) {
 	    nextW(i); // reload
 		return;
 	  }
-    }
+  }
 	
 	var err = w.document.getElementsByClassName('hang_mdash');
     if (err.length > 0) {
       var errText = err[0].innerText;
 	
-	  if ((errText.indexOf(ERR_LOAD) > 0)) { // skip
-	    w.close();
-	    nextW(i - 1); // reload
-		return;
-	  }
+	    if ((errText.indexOf(ERR_LOAD) > 0)) { // skip
+	      w.close();
+	      nextW(i - 1); // reload
+		    return;
+	    }
     }
 
-	try {
-	  w.document.getElementsByClassName('related__wrapper')[0].innerHTML = '';
-	} catch(e) {}
-	w.document.getElementsByClassName('topbuttons')[2].innerHTML = '';
+	  try {
+	    w.document.getElementsByClassName('related__wrapper')[0].innerHTML = '';
+  	} catch(e) {}
+  	w.document.getElementsByClassName('topbuttons')[2].innerHTML = '';
 
     var html = w.document.getElementsByClassName('resume__contacts')[0].innerHTML;
     var email = reEmail.exec(html)[1];
-	email = email.replace(/\+[a-zA-Z0-9._%+-]+@gmail\.com$/g, '@gmail.com');
-	email = email.replace(/[^a-zA-Z0-9._%@-]/g, '');
+	  email = email.replace(/\+[a-zA-Z0-9._%+-]+@gmail\.com$/g, '@gmail.com');
+	  email = email.replace(/[^a-zA-Z0-9._%@-]/g, '');
 
     var text = w.document.getElementsByClassName('resume')[0].innerText;
     w.close();
 
-	text = text.replace(/^\s*|\s*$/g, '').replace(/^\s*|\s*$/gm, '');
-	text = text.replace(RESUME_RENEWED, '$1 $2');
-    res.push([type, name, email, href.replace(/\?.*/, ''), text]);
-    nextW(i - 1);
+	  text = text.replace(/^\s*|\s*$/g, '').replace(/^\s*|\s*$/gm, '');
+	  text = text.replace(RESUME_RENEWED, '$1 $2');
+    pushRes(type, name, email, href.replace(/\?.*/, ''), text, i);
   });
 }
 
