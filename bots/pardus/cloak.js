0804d11e1de008f1404d67daedf5318ce416023b
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2013-05-12 19:09:44 +0000

    A simple cloak trainer to waste APs quickly

---
+++ bots/pardus/cloak.js
@@ -0,0 +1,24 @@
+var TIMEOUT = 500;
+
+function waitFor(f) {
+  setTimeout(function() {
+    try {
+      f();
+    } catch (e) { 
+      console.log('waitFor(): ' + e);
+      waitFor(f);
+    }
+  }, TIMEOUT);
+}
+
+function next() {
+  if (document.getElementById('apsleft').innerHTML < 260) return;
+  
+  waitFor(function() {
+    var e = document.getElementById('inputShipCloak') || document.getElementById('inputShipUncloak');
+	if (!e) throw 'Loading';
+	e.click();
+	next();
+  });
+}
+
