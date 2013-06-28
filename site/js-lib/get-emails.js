7db433c252e684fa3c00b926c5a3b05ecbf6e482
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2013-06-28 22:30:44 +0000

    Better context

--- site/js-lib/get-emails.js
+++ site/js-lib/get-emails.js
@@ -1,17 +1,22 @@
+var _CONTEXT = 20;
+
 function getEmails(content, res, domain) {
   var result;
   
   content = content.replace(/(\s+|_+)at\1/gi, '@');
   
-  var registerEmail = function(email, context) {
-    res[email.toLowerCase()] = context;
+  var registerEmail = function(email, index, lastIndex) {
+    index = (index < _CONTEXT) ? 0 : index - _CONTEXT;
+    var len = lastIndex - index + _CONTEXT * 2;
+    res[email.toLowerCase()] = content.substr(index, len);
   }
   
-  while ((result = /(.{0,20}[^a-z0-9._%+-]|)([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4})/gi.exec(content)) !== null)
-    registerEmail(result[1], result[0]);
+  re = /([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4})/gi;
+  while ((result = re.exec(content)) !== null)
+    registerEmail(result[0], result.index, re.lastIndex);
   
   if (domain) {
-    var re = new RegExp('(.{0,20}[^a-z0-9._%+-]|)([a-z0-9._%+-]+)[^a-z0-9._%+-]+' + domain.replace('.', '\.'), 'gi');
-    while ((result = re.exec(content)) !== null) registerEmail(result[1] + '@' + domain, result[0]);
+    var re = new RegExp('([a-z0-9._%+-]+)[^a-z0-9._%+-]+' + domain.replace('.', '\.'), 'gi');
+    while ((result = re.exec(content)) !== null) registerEmail(result[0] + '@' + domain, result.index, re.lastIndex);
   }
 }
