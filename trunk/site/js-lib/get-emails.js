5aa799f809b7be366debe5c3fdc542f7efb930c7
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2013-06-28 22:16:40 +0000

    Reduced context

--- trunk/site/js-lib/get-emails.js
+++ trunk/site/js-lib/get-emails.js
@@ -7,11 +7,11 @@ function getEmails(content, res, domain) {
     res[email.toLowerCase()] = context;
   }
   
-  while ((result = /(.{0,20}([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}).{0,20})/gi.exec(content)) !== null)
+  while ((result = /(.{0,20}[^a-z0-9._%+-]|)([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4})/gi.exec(content)) !== null)
     registerEmail(result[1], result[0]);
   
   if (domain) {
-    var re = new RegExp('(.{0,20}([a-z0-9._%+-]+)[^a-z0-9._%+-]+' + domain.replace('.', '\.'), 'gi');
+    var re = new RegExp('(.{0,20}[^a-z0-9._%+-]|)([a-z0-9._%+-]+)[^a-z0-9._%+-]+' + domain.replace('.', '\.'), 'gi');
     while ((result = re.exec(content)) !== null) registerEmail(result[1] + '@' + domain, result[0]);
   }
 }
