a7cfc86649c2cc2a0e64d525ad4892cd66716db3
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2013-06-28 21:23:54 +0000

    lowercasing mails

--- trunk/site/js-lib/get-emails.js
+++ trunk/site/js-lib/get-emails.js
@@ -3,10 +3,15 @@ function getEmails(content, res, domain) {
   
   content = content.replace(/(\s+|_+)at\1/gi, '@');
   
-  while ((result = /(.{0,20}([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}).{0,20})/gi.exec(content)) !== null) res[result[1]] = result[0];
+  var registerEmail = function(email, context) {
+    res[email.toLowerCase()] = context;
+  }
+  
+  while ((result = /(.{0,20}([a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}).{0,20})/gi.exec(content)) !== null)
+    registerEmail(result[1], result[0]);
   
   if (domain) {
     var re = new RegExp('(.{0,20}([a-z0-9._%+-]+)[^a-z0-9._%+-]+' + domain.replace('.', '\.'), 'gi');
-	  while ((result = re.exec(content)) !== null) res[result[1] + '@' + domain] = result[0];
+	  while ((result = re.exec(content)) !== null) registerEmail(result[1] + '@' + domain, result[0]);
   }
 }
