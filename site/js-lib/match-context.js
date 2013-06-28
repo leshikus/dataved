9f8d76a91bf33f9147f967b86d7ac7465876c3d1
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2013-06-28 23:06:21 +0000

    v1

--- site/js-lib/match-context.js
+++ site/js-lib/match-context.js
@@ -5,7 +5,7 @@ function matchContext(re, content, cb) {
   while ((result = re.exec(content)) !== null) {
     var match = result[0];
     var len = _CONTEXT * 2 + match.length;
-    var index = re.lastIndex - _CONTEXT - email.length;
+    var index = re.lastIndex - _CONTEXT - match.length;
 	if (index < 0) index = 0;
 	var s = content.substr(index, len);
 	console.log("matched: " + match + " substr: " + s);
