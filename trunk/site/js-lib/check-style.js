b99906b1341c870ba32d949709b0e0ed498a20c5
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2013-06-29 00:51:41 +0000

    v1

--- trunk/site/js-lib/check-style.js
+++ trunk/site/js-lib/check-style.js
@@ -2,10 +2,10 @@ var preRe = /<(pre|code)>[^<]*<\/\1>/gi;
 var scriptRe = /<(script|style|textarea)[^<]*(\n\s*[^<].*)*\s*<\/\1>/gi;
 var entityRe = /&[a-z#0-9]+;/g;
 
-function check(url, content) {
+function checkRules(url, content) {
   var html = content.replace(scriptRe, '');
   html = html.replace(preRe, 'code');
-  html = html.replace(/\.NET|Яндекс\.Фотки|H\.(264|323)/g, 'новояз');
+  html = html.replace(/\.NET|РЇРЅРґРµРєСЃ\.Р¤РѕС‚РєРё|H\.(264|323)/g, 'РЅРѕРІРѕСЏР·');
   html = html.replace(/\d+[,.]\d+/g, '1729');
 
   var text = html.replace(/<\/?(p|div|li|blockquote|br|td|th|tr)(|\s[^<]*)>/g, ' ');
@@ -13,24 +13,24 @@ function check(url, content) {
 
   for (r in rules) {
     var error = '';
-	
+    
     matchHtmlRule = function(re) {
       matchContext(re, html, function(match, context) {
-	    maxErrors--;
+        maxErrors--;
         error += context.replace(/</g, '&lt;').replace(/>/g, '&gt;').
-		  replace(/&/g, '&amp;') + '<br/>';
-	  }
+          replace(/&/g, '&amp;') + '<br/>';
+      }
     }
-	
+    
     matchTextRule = function(re) {
       matchContext(re, text, function(match, context) {
-	    maxErrors--;
+        maxErrors--;
         error += context + '<br/>';
-	  }
+      }
     }
 
-	rules[r]();
-	
-	if (error) e(rule).innerHTML += url + ":<pre>\n" + error '</pre>';
+    rules[r]();
+    
+    if (error) e(rule).innerHTML += url + ":<pre>\n" + error '</pre>';
   }
 }
