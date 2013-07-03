97b5eed2a3605766320d06fbdf70b893e5004b37
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2013-07-03 19:12:21 +0000

    v1

--- site/js-lib/get-emails.js
+++ site/js-lib/get-emails.js
@@ -4,7 +4,7 @@ function getEmails(content, res, domain) {
   var result;
   
   content = content.replace(/\s+/g, ' ');
-  content = content.replace(/[^a-z0-9_.%+-]+at[^a-z0-9.-]+|(_+)at\1/gi, '@');
+  content = content.replace(/[^a-z0-9_.%+-]+at[^a-z0-9.-]+|([_-]+)at\1/gi, '@');
   
   var registerEmail = function(email, s) {
     res[email.toLowerCase()] = s;
