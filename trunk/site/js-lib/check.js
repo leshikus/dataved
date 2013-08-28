61836c64591a17baef95f5ecc6afe9c8e3ed96e4
alexei.fedotov <alexei.fedotov@917aa43a-1baf-11df-844d-27937797d2d2>
2013-08-28 11:13:41 +0000

    0

--- trunk/site/js-lib/check.js
+++ trunk/site/js-lib/check.js
@@ -1,7 +1,7 @@
 var _MASK = 18071973; // this is my birthday
 
 function checkProblem(form, result) {
-  if (this.elements[0] ^ result == _MASK) {
+  if (this.elements[0].value ^ result == _MASK) {
     document.getElementById('hidden').style.display = 'block';
   } else {
     alert('sorry, incorrect');
