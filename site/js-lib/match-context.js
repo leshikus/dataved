d51092b71cf7a06bcadd388a86329d56c1e71a71
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2014-04-22 15:15:53 +0000

    v1

--- site/js-lib/match-context.js
+++ site/js-lib/match-context.js
@@ -6,9 +6,9 @@ function matchContext(re, content, cb) {
     var match = (result.length == 1) ? result[0] : result[1];
     var len = _CONTEXT * 2 + match.length;
     var index = re.lastIndex - _CONTEXT - match.length;
-  	if (index < 0) index = 0;
-	  var s = content.substr(index, len);
-	  console.log("matched: " + match + " substr: " + s);
+    if (index < 0) index = 0;
+    var s = content.substr(index, len);
+    console.log("matched: " + match + " substr: " + s);
     cb(match, s);
   }
 }
