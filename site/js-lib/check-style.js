988969857c668ea4c40a6f5eff3db2ee2e6ffeaf
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2014-04-22 16:13:46 +0000

    v1

--- site/js-lib/check-style.js
+++ site/js-lib/check-style.js
@@ -2,7 +2,24 @@ var preRe = /<(pre|code)>[^<]*<\/\1>/gi;
 var scriptRe = /<(script|style|textarea)[^<]*(\n\s*[^<].*)*\s*<\/\1>/gi;
 var entityRe = /&[a-z#0-9]+;/g;
 
-function checkRules(e, rules, url, content) {
+function Matcher(data, log) {
+  this.data = data;
+  this.log = log;
+  
+  this.cut = function(re) {
+    this.data = this.data.replace(re, '');
+  }
+  
+  this.match = function(re) {
+    matchContext(re, this.data, function(match, context) {
+      maxErrors--;
+      this.log(context);
+    });
+  }    
+}
+
+
+function checkRules(rules, content, log) {
   var html = content.replace(scriptRe, '');
   html = html.replace(preRe, 'code');
   html = html.replace(/\.NET|Яндекс\.Фотки|H\.(264|323)/g, 'новояз');
@@ -11,33 +28,14 @@ function checkRules(e, rules, url, content) {
   var text = html.replace(/<\/?(p|div|li|blockquote|br|td|th|tr)(|\s[^<]*)>/g, ' ');
   text = text.replace(/<[^<]*>/g, '');
 
+  var getHtml = function() {
+    return new Matcher(html, log);
+  }
+  var getText = function() {
+    return new Matcher(text, log);
+  }
 
   for (r in rules) {
-    var error = '';
-
-    cutHtml = function(re) {
-      html = html.replace(re, '');
-    }
-
-    cutText = function(re) {
-      text = text.replace(re, '');
-    }
-    
-    matchHtmlRule = function(re) {
-      matchContext(re, html, function(match, context) {
-        maxErrors--;
-        error += context + '<br/>';
-      });
-    }
-    
-    matchTextRule = function(re) {
-      matchContext(re, text, function(match, context) {
-        maxErrors--;
-        error += context + '<br/>';
-      });
-    }
-
     rules[r]();
-    if (error) e(r).innerHTML += url + ":<pre>\n" + error + '</pre>';
   }
 }
