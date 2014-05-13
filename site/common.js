cee31e2af899e0181bae2239599072a7737c1a5f
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@917aa43a-1baf-11df-844d-27937797d2d2>
2014-05-13 08:56:05 +0000

    Removed facebook sharing

--- site/common.js
+++ site/common.js
@@ -1,11 +1,3 @@
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {return;}
-  js = d.createElement(s); js.id = id;
-  js.src = "http://connect.facebook.net/ru_RU/all.js#xfbml=1";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 var _gaq;
 
 $(document).ready(function() {
